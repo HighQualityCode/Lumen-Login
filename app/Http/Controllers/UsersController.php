98cbe0a1c304c55e0afecb962e187c948cb246c7
HighQualityCode <priceofjs@gmail.com>
2018-02-09 04:54:52 -0600

    clean code

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,22 +17,20 @@ class UsersController extends Controller {
    public function authenticate(Request $request)
    {
   		$user = User::where('username', $request->input('username'))->first();
-
-
       if ($user && (Hash::check($request->input('password'), $user->password)))
-        {
-          $apikey = base64_encode(str_random(40));
-          User::where('username', $request->input('username'))->update(['api_key' => "$apikey"]);
-          return response()->json(['status' => 'success','token' => $apikey, 
-                                   'firstname' => $user->firstname, 
-                                   'lastname' => $user->lastname,
-                                  ]);
-        }
+      {
+        $apikey = base64_encode(str_random(40));
+        User::where('username', $request->input('username'))->update(['api_key' => "$apikey"]);
+        return response()->json([
+          'status' => 'success','token' => $apikey, 
+          'firstname' => $user->firstname, 
+          'lastname' => $user->lastname,
+        ]);
+      }
       else 
       {
         return response()->json(['message'=>'Your email or Password icorrect'], 401);
       }
-
    }
 
    public function register(Request $request) {
